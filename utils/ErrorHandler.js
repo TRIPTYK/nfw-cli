84b4e718444c3ac85bcb798a92cf2660e05a9ac7
Dramix <romain.verliefden@std.heh.be>
2019-03-06 15:34:55 +0100

    migrate error handler

---
+++ utils/ErrorHandler.js
@@ -0,0 +1,27 @@
+const operatingSystem = process.platform;
+const fs = require('fs');
+const  sql = require('../generate/database/sqlAdaptator');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const chalk = require('chalk');
+
+
+exports.migrateRunFail = async () =>{
+    let files = fs.readdirSync('./dist/migration/');
+    let regex = RegExp('[^0-9]+');
+    let migrationDone = [];
+    let migration = await sql.Select(['timestamp','name'],'migration_table');
+    console.log(chalk.red("Please check your model then run nfw migrate"));
+    migration.forEach(mig => {
+      let migName = regex.exec(mig.name);
+      let migDone = mig.timestamp+"-"+migName[0]+".js";
+        migrationDone.push(migDone);
+    });
+    files.forEach(file =>{
+        if (!migrationDone.includes(file) && file !== 'dump')operatingSystem === 'win32'  ? exec(`DEL /Q /S dist\\migration\\${file}`) : exec(`rm -rf ./dist/migration/${file}`);
+    })  
+
+
+exports.deleteMigrateErr = (err) =>{
+    console.log("Could not delete non-compiled migration because :"+err.message);
+}}
