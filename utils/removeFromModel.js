5611e5cb4aa0f47090036382abdad057d7b2655c
Dramix <romain.verliefden@std.heh.be>
2019-04-12 13:49:30 +0200

    remove relation fixed

--- utils/removeFromModel.js
+++ utils/removeFromModel.js
@@ -4,24 +4,30 @@ const FS = require('fs');
 const ReadFile = Util.promisify(FS.readFile);
 const WriteFile = Util.promisify(FS.writeFile);
 const { lowercaseEntity} = require('../generate/utils');
-const pluralize = require('pluralize');
+const {singular,isSingular,plural}= require('pluralize');
 
  /**
    * @description  Remove relationship from serializer and controller
    * @param {string} model Model name
    * @param {string} column relation name
    */
-exports._removefromSandC =async  (entity,column) =>{
-    let regexAddRel = new RegExp(`.addRelation\\('${column}[\\s\\S]*?\\)`);
+exports.removefromRelationTable =async  (entity,column) =>{  
     let regexArray = new RegExp(`,'${column}'|'${column}',|'${column}'`,'m');
-    let serializer = `${process.cwd()}/src/api/serializers/${pluralize.singular(entity)}.serializer.ts`;
-    let serializerContent = await ReadFile(serializer, 'utf-8');
-    let relation= `${process.cwd()}/src/api/enums/relations/${pluralize.singular(entity)}.relations.ts`;
+    let relation= `${process.cwd()}/src/api/enums/relations/${singular(entity)}.relations.ts`;
     let relationContent = await ReadFile(relation, 'utf-8');
+    newRel = relationContent.replace(regexArray,'');
+    await WriteFile(relation,newRel);
+  }
+
+exports.removeFromSerializer = async (entity,column) =>{
+    let serializer = `${process.cwd()}/src/api/serializers/${singular(entity)}.serializer.ts`;
+    let serializerContent = await ReadFile(serializer, 'utf-8');
+    let regexAddRel = new RegExp(`${column} :[\\s\\S]*?},`);
+    let regexArray = new RegExp(`,'${column}'|'${column}',|'${column}'`,'m');
     let newSer= serializerContent.replace(regexAddRel,'');
     newSer = newSer.replace(regexArray,'');
-    newRel = relationContent.replace(regexArray,'');
-    await Promise.all([WriteFile(serializer,newSer),WriteFile(relation,newRel)]);
+    if(isSingular(column)) newSer = newSer.replace(new RegExp(`${plural(column)} :[\\s\\S]*?},`),'');
+    await WriteFile(serializer,newSer)
   }
 
   const removeFromTest = async (model,column) =>{
@@ -52,13 +58,13 @@ exports._removefromSandC =async  (entity,column) =>{
     let regexColumn =  new RegExp(`@Column\\({[\\s\\S][^{]*?${column};`,'m');
     let regexMany = new RegExp(`@Many[\\s\\S][^;]*?${column} :.*`);
     let regexOne = new RegExp(`@One[\\s\\S][^;]*?${column} :.*`);
-    let pathModel = `${process.cwd()}/src/api/models/${pluralize.singular(model)}.model.ts`;
+    let pathModel = `${process.cwd()}/src/api/models/${singular(model)}.model.ts`;
     let modelFile = await ReadFile(pathModel);
     let newModel;
     if(modelFile.toString().match(regexColumn)) newModel=modelFile.toString().replace(regexColumn,'');
     else if(modelFile.toString().match(regexMany))newModel=modelFile.toString().replace(regexMany,'');
     else if(modelFile.toString().match(regexOne)) newModel=modelFile.toString().replace(regexOne,'');
     else throw new Error('Column doesn\'t exist');
-    await Promise.all([WriteFile(pathModel,newModel),removeFromTest(model,column),removeFromValidation(model,column)])
+    await Promise.all([WriteFile(pathModel,newModel)/*,removeFromTest(model,column),removeFromValidation(model,column)*/])
   }
   
