5a9648c73de0ceea51cd68305712d5b72f1be549
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-04-15 13:52:19 +0100

    minor code refactoring

--- utils/ErrorHandler.js
+++ utils/ErrorHandler.js
@@ -3,30 +3,34 @@
  * @exports migrateRunFail
  * @exports deleteMigrateErr
  */
-const operatingSystem = process.platform;
 const fs = require('fs');
-const  sql = require('../generate/database/sqlAdaptator');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const chalk = require('chalk');
 
+const sql = require('../generate/database/sqlAdaptator');
+
+const operatingSystem = process.platform;
 
 exports.migrateRunFail = async () =>{
     let files = fs.readdirSync('./dist/migration/');
-    let regex = RegExp('[^0-9]+');
-    let migrationDone = [];
+    let regex = /[^0-9]+/;
     let migration = await sql.Select(['timestamp','name'],'migration_table');
-    console.log(chalk.red("Please check your model then run nfw migrate"));
-    migration.forEach(mig => {
+
+    Log.error("Please check your model then run nfw migrate");
+
+    let migrationFiles = migration.map(mig => {
       let migName = regex.exec(mig.name);
-      let migDone = mig.timestamp+"-"+migName[0]+".js";
-        migrationDone.push(migDone);
+       return `${mig.timestamp}-${migName[0]}.js`;
     });
+
     files.forEach(file =>{
-        if (!migrationDone.includes(file) && file !== 'dump')operatingSystem === 'win32'  ? exec(`DEL /Q /S dist\\migration\\${file}`) : exec(`rm -rf ./dist/migration/${file}`);
-    })  
+        if (!migrationFiles.includes(file) && file !== 'dump')
+          fs.unlinkSync(`./dist/migration/${file}`);
+    });
+}
 
 
-exports.deleteMigrateErr = (err) =>{
-    console.log("Could not delete non-compiled migration because :"+err.message);
-}}
+exports.deleteMigrateErr = (err) => {
+    console.log("Could not delete non-compiled migration because :" + err.message);
+}
