7740c33ba37dece8a3b9e69c94a9aba2cc3e9721
DEFLORENNE Amaury <amaury.deflorenne@std.heh.be>
2019-04-16 11:19:10 +0100

    start & info command

---
+++ commands/infoCommand.js
@@ -0,0 +1,22 @@
+/**
+ * node modules
+ */
+const yargs = require('yargs');
+const chalk = require('chalk');
+
+exports.command = 'info';
+exports.aliases = ['i'];
+
+exports.describe = 'Show the information about the developers';
+
+exports.builder = (yargs) => {}
+
+exports.handler = (argv) => {
+  console.log(
+    chalk.bgGreen('Made by :') +
+    "\n Amaury Deflorenne <https://github.com/AmauryD>" +
+    "\n Romain Verliefden <https://github.com/DramixDW>" +
+    "\n Samuel Antoine <https://github.com/Snorkell>" +
+    "\n Steve Lebleu <https://github.com/konfer-be>"
+  );
+}
