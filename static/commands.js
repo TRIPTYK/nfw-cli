b03075fe01d5240aa576f9c958bc274e126cf133
Snorkell <samgaming29@live.fr>
2019-04-19 10:47:34 +0200

    Replace all tsc with node_modules/.bin/tsc

--- static/commands.js
+++ static/commands.js
@@ -6,25 +6,25 @@ module.exports = {
     getNPMCommandsUnix: {
         currentDirectory: "cd ",
         kickstart: "mkdir -p ./dist/logs ./dist/migration/dump ./dist/uploads/documents/xs ./dist/uploads/documents/md ./dist/uploads/documents/xl && npm i bcrypt --save && npm i typescript && npm i typeorm && npm i && ./node_modules/.bin/tsc",
-        compileTypeScript: "tsc",
+        compileTypeScript: "./node_modules/.bin/tsc",
         test: "npm run testUnix"
     },
     getNPMCommandsWindows: {
         currentDirectory: "cd ",
         kickstart: "init_scripts\\windowsnpm.bat",
-        compileTypeScript: "tsc",
+        compileTypeScript: "node_modules\\.bin\\tsc",
         test: "npm run testWin"
     },
     getYarnCommandsUnix: {
         currentDirectory: "cd ",
         kickstart: "mkdir -p ./dist/logs ./dist/migration/dump ./dist/uploads/documents/xs ./dist/uploads/documents/md ./dist/uploads/documents/xl && yarn add bcrypt --dev && yarn add typescript --dev && yarn add typeorm --dev && yarn && ./node_modules/.bin/tsc",
-        compileTypeScript: "tsc",
+        compileTypeScript: "./node_modules/.bin/tsc",
         test: "yarn run testUnix"
     },
     getYarnCommandsWindows: {
         currentDirectory: "cd ",
         kickstart: "init_scripts\\windowsyarn.bat",
-        compileTypeScript: "tsc",
+        compileTypeScript: "node_modules\\.bin\\tsc",        
         test: "yarn run testWin"
     }
 };
