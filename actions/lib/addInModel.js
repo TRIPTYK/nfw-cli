09b4cc4093a95a3154ff5b5b9759222491be195f
Dramix <romain.verliefden@std.heh.be>
2019-04-30 16:58:26 +0200

    indent in edit command

--- actions/lib/addInModel.js
+++ actions/lib/addInModel.js
@@ -3,6 +3,7 @@ const Util = require('util');
 const Log = require('../../utils/log');
 const FS = require('fs');
 const chalk = require('chalk');
+const kebab = require('kebab-case')
 
 const ReadFile = Util.promisify(FS.readFile);
 const WriteFile = Util.promisify(FS.writeFile);
@@ -26,7 +27,7 @@ const addToValidations = async (model,column) =>{
     if(col.length && col.baseType !== 'any') toPut += `.max(${col.length})`;
     col.baseColumn.Null !== 'NO' && col.baseColumn.Default !== 'NULL' ? toPut+='.required(),' : toPut+='.optional(),';
 
-    if(!valFile.match(regexRandom))valFile = valFile.replace(regexVal,`$1 ${toPut}\n$2 `);
+    if(!valFile.match(regexRandom))valFile = valFile.replace(regexVal,`$1 ${toPut}$2 `);
     await WriteFile(valPath,valFile).then(() => Log.info(`${chalk.cyan(`src/api/validations/${model}.validation.ts`)} updated`))
 };
 
@@ -45,18 +46,18 @@ const addToTest = async (model, column) => {
 
     //regex to hand 'column' in the .body to include all keys
     //write 'column' or ,'column' if there is already columns in the list
-    let rgxList = new RegExp(`(expect\\(res.body[\\s\\S]*?\\()()([^);]*)`,'gm');
+    let rgxList = new RegExp(`(expect\\(res.body[\\s\\S]*?')(\\n)`,'gm');
     let regexMatch = testFile.match(rgxList);
     let toPutInList;
-    if (regexMatch[2].includes('\'')) toPutInList = `,'${column.Field}'`;
-    else toPutInList = `${column.Field}`;
+    if (regexMatch[2].includes('\'')) toPutInList = `,'${kebab(column.Field)}'`;
+    else toPutInList = `${kebab(column.Field)}`;
 
 
     //put the string in the file if not already present then write
     let regexRandom = new RegExp(`[\\s]${column.Field}.*?,`, 'gm');
     let regexArray = new RegExp(`,'${column.Field}'|'${column.Field}',|'${column.Field}'`, 'gm');
     if(!testFile.match(regexRandom))testFile = testFile.replace(rgxRandomType,`$1${toPutRandType}\n$3`);
-    if(!testFile.match(regexArray))testFile = testFile.replace(rgxList,`$1$2$3${toPutInList}`);
+    if(!testFile.match(regexArray))testFile = testFile.replace(rgxList,`$1${toPutInList}\n`);
     await WriteFile(testPath,testFile).then(() => Log.info(`${chalk.cyan(`test/${model}.test.ts`)} updated`));
 
 };
