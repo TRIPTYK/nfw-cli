95e2e38f3e293e1b179022f5b0c411a67ec2fea6
Billy <glenn.honore@std.heh.be>
2020-02-17 10:54:09 +0100

    Conversion: createSuperUserCommand => .ts

---
+++ src/commands/createSuperUserCommand.ts
@@ -0,0 +1,69 @@
+/**
+ * @module createSuperUserCommand
+ * @description Creates a super user in the boilerplate database
+ * @author Deflorenne Amaury
+ */
+
+//Node modules imports
+import chalk from 'chalk';
+
+// Project imports
+import commandUtils = require('./commandUtils');
+import Log = require('../utils/log');
+import createSuperUserAction = require('../actions/createSuperUserAction');
+
+
+//Yargs command syntax
+export const command: string = 'createUser <username>';
+
+//Yargs command aliases
+export const aliases: string[] = ['cu'];
+
+//Yargs command description
+export const describe: string = 'Create a Super User and save the credentials in a file';
+
+//Yargs command builder
+export function builder (yargs: any) {
+    yargs.option('mail',{
+        alias: 'e',
+        type: 'string',
+        description: 'Email address of the user'
+    });
+    yargs.option('role',{
+        alias: 'r',
+        type: 'string',
+        description: 'Role of the user (ex : admin,user,ghost)'
+    });
+    yargs.option('password',{
+        alias: 'p',
+        type: 'string',
+        description: 'Password of the user'
+    });
+};
+
+//Main function
+exports.handler = async (argv: any) => {
+    const {username,mail,role,password} = argv;
+
+    commandUtils.validateDirectory();
+    await commandUtils.checkConnectToDatabase();
+
+    commandUtils.updateORMConfig();
+
+    await new createSuperUserAction.CreateSuperUSerActionClass(username,mail,role,password).Main()
+        .then((generated) => {
+            const [ filePath ] = generated;
+
+            Log.info(`Created ${filePath}`);
+
+            console.log(
+                chalk.bgYellow(chalk.black('/!\\ WARNING /!\\ :')) +
+                `\nYou have generated a ${role} user named ${chalk.red(username)} for your API, the credentials are written in the file named \"credentials.json\" located int he root folder, please modify the password as soon as possible, we are not responsible if someone finds it, it is your responsibility to change this password to your own password`
+            );
+        })
+        .catch((e) => {
+            Log.error("Failed to create super user : " + e.message);
+        });
+
+    process.exit(0);
+};
