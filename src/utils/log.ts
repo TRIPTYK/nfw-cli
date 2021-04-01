e6d8786a3c8288b3cfc61589c6aa84a288c325b4
teaflex <teaflex.dev@hotmail.com>
2021-04-01 11:37:50 +0200

    log upgrade

---
+++ src/utils/log.ts
@@ -0,0 +1,72 @@
+/**
+ * @module log
+ * @description Shortcut module to log colorful text messages
+ * @author Deflorenne Amaury
+ */
+import * as Chalk from "chalk";
+
+/**
+ * Static logger class.
+ */
+export class Logger {
+    private static types = {
+        error: [Chalk.bold.red, '❌'],
+        warning: [Chalk.yellow, '⚠'],
+        success: [Chalk.green, '✔'],
+        info: [Chalk.blue, 'i'],
+    } as any;
+
+    /**
+     * Error message.
+     * @param text Text to display.
+     */
+    static error(text: string) {
+        this.basic("error", text);
+    };
+
+    /**
+     * Warning message.
+     * @param text Text to display.
+     */
+    static warning(text: string) {
+        this.basic("warning", text);
+    };
+
+    /**
+     * Success message.
+     * @param text Text to display.
+     */
+    static success(text: string) {
+        this.basic("success", text);
+    };
+
+    /**
+     * Info message.
+     * @param text Text to display.
+     */
+    static info(text: string) {
+        this.basic("info", text);
+    };
+
+    /**
+     * Basic message.
+     * @param type Type of the message.
+     * @param text Text to display.
+     */
+    static basic(type: MessageType, text: string) {
+        this.custom(text, this.types[type][1], this.types[type][0]);
+    }
+
+    /**
+     * Customizable message.
+     * @param text Text to display.
+     */
+    static custom(text: string, symbol = "", color = Chalk.white) {
+        if(symbol.length)
+            symbol = `[${symbol}]`;
+        console.log(`${color(symbol)} ${text}`);
+    }
+
+}
+
+export type MessageType = "success" | "warning" | "error" | "info";
