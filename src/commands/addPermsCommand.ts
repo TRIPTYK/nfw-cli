212defefa9cbc817f235781ec0332ac3220fdfee
BaptisteD <baptiste.dambrin@gmail.com>
2021-04-06 15:06:22 +0200

    add perm command

--- src/commands/addPermsCommand.ts
+++ src/commands/addPermsCommand.ts
@@ -1,26 +1,36 @@
 import { BaseCommand } from "./template";
 import * as inquirer from "inquirer";
-import {
-	addRole,
-	save,
-	getEntityRoutes,
-	MetadataController,
-} from "@triptyk/nfw-core";
+import { getRoles, addPerms, save } from "@triptyk/nfw-core";
 import { Logger as log } from "../utils/log";
 
 export class AddPermsCommand extends BaseCommand {
-	public command = "add-perms";
+	public command = "add-perms <entity> <route> <requestMethod>";
 	public aliases = ["adper"];
 
 	async handler(argv: any) {
+		const roles = await getRoles();
+		roles.push("--Cancel--");
 		inquirer
 			.prompt([
 				{
-					name: "addRole",
-					type: "input",
-					message: "Choose a name for the role : ",
+					name: "addPerm",
+					type: "list",
+					message: `What role do you want to add to ${argv.route}`,
+					choices: roles,
 				},
 			])
-			.then((answer) => {});
+			.then((answer) => {
+				if (answer.deleteRole !== "Cancel") {
+					const entity = {
+						entity: argv.entity,
+						methodName: argv.route.split("/")[1],
+						requestMethod: argv.requestMethod,
+						path: argv.route,
+						role: answer.addPerm,
+					};
+					addPerms(entity);
+					save();
+				}
+			});
 	}
 }
