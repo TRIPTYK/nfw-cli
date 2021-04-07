48872d82ca1abaab0eca0d53fa018d3057e0061a
teaflex <teaflex.dev@hotmail.com>
2021-04-07 12:30:47 +0200

    delEndpoint

---
+++ src/commands/delEndpointCommand.ts
@@ -0,0 +1,18 @@
+import { removeEndpoint, save } from "@triptyk/nfw-core";
+import { Logger as Log } from "../utils";
+import { BaseCommand } from "./template";
+
+export class DelEndpointCommand extends BaseCommand {
+    public command: string | string[] = "del-endpoint <prefix> <tsmethod>";
+    public aliases = ["delend"]
+    public describe = "Delete an endpoint of a specific route."
+
+    public async handler(argv: any) {
+
+        await removeEndpoint(argv.prefix, argv.tsmethod);
+
+        await save();
+
+        Log.success(`Endpoint /${argv.prefix}/${argv.endpoint} created !`);
+    }
+}
