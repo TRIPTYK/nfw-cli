87fb57e0ee1f242c48bdc62a881ca5849a0b776b
BaptisteD <baptiste.dambrin@gmail.com>
2021-04-15 17:00:22 +0200

    test

--- src/test/command.test.ts
+++ src/test/command.test.ts
@@ -1,6 +1,18 @@
-// import * as chai from "chai";
-import * as AddRoleCommand from "../commands/AddRoleCommand";
+import { CommandsRegistry } from "../application";
+import { getRoles } from "@triptyk/nfw-core";
+import { assert } from "chai";
 
 describe("Commands", () => {
-	it("Add Role command", () => {});
+	before(async () => {
+		const roles = ["Admin", "User", "Ghost"];
+	});
+
+	it("Add Role command", async () => {
+		// const argv = { roleName: "lapin" };
+		// await CommandsRegistry.all.AddRoleCommand.handler(argv);
+
+		const answer = { deleteRole: "lapin" };
+		await CommandsRegistry.all.DelRoleCommand.handler(answer);
+		// console.log(CommandsRegistry.all);
+	});
 });
