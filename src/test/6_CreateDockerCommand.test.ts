034d86a6aa0181ceb10c1b5aabb874a503b515a2
teaflex <teaflex.dev@hotmail.com>
2021-04-29 10:31:18 +0200

    tests for createDocker

--- src/test/6_CreateDockerCommand.test.ts
+++ src/test/6_CreateDockerCommand.test.ts
@@ -1,5 +1,42 @@
+import { expect } from "chai";
+import { promisifiedExec as exec } from "../utils";
 import * as global from "./global"
 
+// Please note: to run these tests, you must 
+// have your docker deamon turned on.
+
 describe("CreateDockerCommand", function() {
-	//Your test here :)
+	this.timeout("3600s");
+
+	let command = null;
+	const container = "test";
+
+	afterEach(async () => {
+		await global.cleanDocker(container);
+	});
+
+	it("Creates a container", async () => {
+		command = await exec(`nfw docker ${container}`);
+
+		expect(command).to.contain(`Container "${container}" created with success`);
+		expect(command).to.contain(`Your container "${container}" is now running and listening to port 3306.`);
+	});
+
+	it("Creates a personnalized container", async () => {
+		const port = 3307;
+		const user = "Boby";
+		const password = "secret";
+
+		command = await exec(`nfw docker ${container} --port=${port} --user=${user} --password=${password} --nativePassword`);
+
+		expect(command).to.contain(`Container "${container}" created with success`);
+		expect(command).to.contain(`Your container "${container}" is now running and listening to port ${port}.`);
+	});
+
+	it("Creates a container without running it", async () => {
+		command = await exec(`nfw docker ${container} --noRun`);
+
+		expect(command).to.contain(`Container "${container}" created with success`);
+		expect(command).to.not.contain(`Your container "${container}" is now running and listening to port 3306.`);
+	});
 });
