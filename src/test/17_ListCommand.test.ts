77031f574cacc14a5a00c55973cd3760b6be17d9
teaflex <teaflex.dev@hotmail.com>
2021-04-30 10:50:49 +0200

    list command tests

--- src/test/17_ListCommand.test.ts
+++ src/test/17_ListCommand.test.ts
@@ -1,5 +1,27 @@
-import * as global from "./global"
+import { expect } from "chai";
+import { execInProject as exec } from "./global"
 
 describe("ListCommand", function() {
-	//Your test here :)
+
+	this.timeout("10s");
+
+	let command = null;
+	const kind = "routes";
+
+	it("Lists a bunch of things", () => {
+		command = exec(`nfw ls ${kind}`);
+
+		expect(command).to.exit.with.code(0)
+		.and.stdout.not.empty;
+	});
+
+	it("Tries to list a non-existing kind", () => {
+
+		const faultyValue = "bananas";
+
+		command = exec(`nfw ls ${faultyValue}`);
+
+		expect(command).to.exit.with.code(1)
+		.and.stdout.to.contain("Unvalid kind.");
+	});
 });
