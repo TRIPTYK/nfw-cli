9998f480d3c190ad4072fbace4c6a1264180a010
teaflex <teaflex.dev@hotmail.com>
2021-04-30 11:54:40 +0200

    add/del role tests

--- src/test/12_DelRoleCommand.test.ts
+++
@@ -1,5 +0,0 @@
-import * as global from "./global"
-
-describe("DelRoleCommand", function() {
-	//Your test here :)
-});
--- src/test/8_AddRoleCommand.test.ts
+++ src/test/8_AddRoleCommand.test.ts
@@ -1,18 +1,23 @@
 import { expect } from "chai";
-import { promisifiedExec as exec } from "../utils";
 import { execInProject, testInput } from "./global";
 
 describe("AddRoleCommand", function () {
 	this.timeout("3600s");
 	let command = null;
 
-	it("Add a valid role", async () => {
+	it("Adds a valid role", async () => {
 		command = execInProject(`nfw add-role ${testInput}`);
 		expect(command).to.exit.with.code(0)
 		.and.stdout.to.contain("Role successfully added");
 	});
+
+	it("Tries to add an existing role", async () => {
+		command = execInProject(`nfw add-role ${testInput}`);
+		expect(command).to.exit.with.code(1)
+		.and.stdout.to.contain(`${testInput} already exist`);
+	});
 	
-	it("Add an invalid role", async () => {
+	it("Tries to add an invalid role", async () => {
 
 		const faultyValue = "123=t=gfg=gzeg";
 
