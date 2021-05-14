1a5dbceffe1838fc4907b941410c832adb63cf08
amauryD <deflorenne.amaury@gmail.com>
2021-05-14 10:30:07 +0200

    remove mirage model gen

--- dist/commands/generateMirageModelCommand.js
+++ dist/commands/generateMirageModelCommand.js
@@ -4,48 +4,12 @@
  * @description Command module to handle generating ember data model
  * @author Deflorenne Amaury
  */
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = exports.builder = exports.describe = exports.aliases = exports.command = void 0;
 // Project imports
-var commandUtils = require("./commandUtils");
-var Log = require("../utils/log");
-var generateMirageModelAction_1 = require("../actions/generateMirageModelAction");
+const commandUtils = require("./commandUtils");
+const Log = require("../utils/log");
+const generateMirageModelAction_1 = require("../actions/generateMirageModelAction");
 //Yargs command
 exports.command = 'generateMirageData [model]';
 //Yargs command aliases
@@ -66,29 +30,18 @@ function builder(yargs) {
 exports.builder = builder;
 ;
 //Main function
-function handler(argv) {
-    return __awaiter(this, void 0, void 0, function () {
-        var model, rows, exclude;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    commandUtils.validateDirectory();
-                    model = argv.model, rows = argv.rows, exclude = argv.exclude;
-                    return [4 /*yield*/, generateMirageModelAction_1.default(model, rows, exclude.split(','))
-                            .then(function () {
-                            Log.success("Please copy your clipboard to mirage/fixtures/");
-                        })
-                            .catch(function (e) {
-                            console.log(e);
-                            Log.error('Failed to generate model ' + e.message);
-                        })];
-                case 1:
-                    _a.sent();
-                    process.exit(0);
-                    return [2 /*return*/];
-            }
-        });
+async function handler(argv) {
+    commandUtils.validateDirectory();
+    const { model, rows, exclude } = argv;
+    await generateMirageModelAction_1.default(model, rows, exclude.split(','))
+        .then(() => {
+        Log.success(`Please the content of created folder ./fixtures to mirage/fixtures/`);
+    })
+        .catch((e) => {
+        console.log(e);
+        Log.error('Failed to generate model ' + e.message);
     });
+    process.exit(0);
 }
 exports.handler = handler;
 ;
